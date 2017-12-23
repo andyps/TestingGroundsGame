44a7a523144d8df06a3328965c49d09e534fb124
Andrey Pashentsev <avp200681@gmail.com>
2017-12-23 21:56:52 +0200

    add patrol component instead guard class

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,18 +3,31 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+//#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// TODO protect agains empty values
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!PatrolRoute)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	// auto Guard = Cast<APatrollingGuard>(ControlledPawn);
+	// auto PatrolPoints = Guard->PatrolPointsCPP;
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
+
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	UE_LOG(LogTemp, Warning, TEXT("ExecuteTask %i"), Index);
-
-	auto AIController = OwnerComp.GetAIOwner();
-	auto Guard = Cast<APatrollingGuard>(AIController->GetPawn());
-	auto PatrolPoints = Guard->PatrolPointsCPP;
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
 	auto NextIndex = (Index + 1) % PatrolPoints.Num();
